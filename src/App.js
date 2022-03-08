bf62d502c357dc7ba530eb6e2bed660b4e23a2b2
Miguel Tejera <miguel.tejera.45@gmail.com>
2022-03-08 23:41:21 +0900

    Calculator 2.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Calculator from "./components/Calculator"
 
-function App() {
+export default function App() {
+
+  const [style, setStyle] = React.useState(false);
+  const [cleaner, setCleaner] = React.useState(false);
+
+  let calCulatorInput;
+  let result;
+
+  function getNumByKeyBoard(event){
+    
+    if(cleaner === true){
+      document.getElementById("screen").value = ""
+      setCleaner(false)
+    }
+    document.getElementById("screen").value += event.target.value
+    calCulatorInput = document.getElementById("screen").value
+    
+  }
+
+  function getOperatorByKeyBoard(event){
+    document.getElementById("screen").value += event.target.value
+    calCulatorInput = document.getElementById("screen").value
+    setCleaner(false)
+
+  }
+
+  function getNumByScreen(event){
+    calCulatorInput += event.target.value
+  }
+
+  function getResult(){
+    try{
+      result = (eval(calCulatorInput))
+      document.getElementById("screen").value = result
+      setCleaner(true)
+    }
+    catch{
+        document.getElementById("screen").setAttribute("class", "error")
+      setTimeout(()=> {
+        document.getElementById("screen").setAttribute("class", "display")
+      },1000)
+    }
+  }
+
+  function calculatorStyle(){
+    setStyle(!style)
+  }
+
+  function clear(){
+    document.getElementById("screen").value = ""
+    calCulatorInput = ""
+    result = ""
+  }
+
+  function erase(){
+    document.getElementById("screen").value = document.getElementById("screen").value.slice(0,-1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calculator
+        getNumByKeyBoard={getNumByKeyBoard}
+        getNumByScreen={getNumByScreen}
+        getOperatorByKeyBoard={getOperatorByKeyBoard}
+        getResult={getResult}
+        clear={clear}
+        erase={erase}
+        calculatorStyle={calculatorStyle}
+        style={style}
+      />
     </div>
   );
 }
 
-export default App;
---
+++ src/components/Calculator.js
@@ -0,0 +1,132 @@
+
+import React from "react";
+import "../style.css"
+
+export default function Calculator({
+    calculatorStyle, 
+    style, 
+    getNumByScreen, 
+    getNumByKeyBoard, 
+    getOperatorByKeyBoard,
+    getResult,
+    clear,
+    erase})
+  { 
+      
+    return (
+      <div className={style? "container": "container-dark"}>
+
+
+        <input 
+          id="screen"
+          className={style? "display" : "display-dark"}         
+          onChange={(event)=> getNumByScreen(event)}>
+        </input>
+  
+        <div className="keysContainer">
+  
+          <div className="num">
+  
+            <button 
+                className={style? "buttonOperator": "buttonOperator-dark"} 
+                onClick={clear}>AC</button>
+            
+            <button 
+                className={style? "buttonC": "buttonC-dark"} 
+                onClick={erase}>C</button>
+            <button 
+                className={style? "buttonStyle": "buttonStyle-dark"} 
+                onClick={(event)=>calculatorStyle(event)}>{style? "☼" : "☀︎"}</button>
+  
+            <button 
+                className={style? "button": "button-dark"}
+                value="7" 
+                onClick={(event)=>getNumByKeyBoard(event)}>7</button>
+    
+            <button 
+                className={style? "button": "button-dark"}
+                value="8" 
+                onClick={(event)=>getNumByKeyBoard(event)}>8</button>
+  
+            <button 
+                className={style? "button": "button-dark"}
+                value="9" 
+                onClick={(event)=>getNumByKeyBoard(event)}>9</button>
+  
+            <button 
+                className={style? "button": "button-dark"}
+                value="4" 
+                onClick={(event)=>getNumByKeyBoard(event)}>4</button>
+  
+            <button 
+                className={style? "button": "button-dark"}
+                value="5" 
+                onClick={(event)=>getNumByKeyBoard(event)}>5</button>
+  
+            <button 
+                className={style? "button": "button-dark"}
+                value="6" 
+                onClick={(event)=>getNumByKeyBoard(event)}>6</button>
+  
+            <button 
+                className={style? "button": "button-dark"}
+                value="1" 
+                onClick={(event)=>getNumByKeyBoard(event)}>1</button>
+  
+            <button 
+                className={style? "button": "button-dark"}
+                value="2" 
+                onClick={(event)=>getNumByKeyBoard(event)}>2</button>
+  
+            <button 
+                className={style? "button": "button-dark"}
+                value="3" 
+                onClick={(event)=>getNumByKeyBoard(event)}>3</button>
+  
+          </div>
+  
+          <div className="operators">
+  
+            <button 
+                className={style? "buttonOperator": "buttonOperator-dark"} 
+                value="*"         
+                onClick={(event)=>getOperatorByKeyBoard(event)}>×</button>
+  
+            <button 
+                value="/" 
+                className={style? "buttonOperator": "buttonOperator-dark"}         
+                onClick={(event)=>getOperatorByKeyBoard(event)}>÷</button>
+  
+            <button 
+                value="+" 
+                className={style? "buttonOperator": "buttonOperator-dark"}         
+                onClick={(event)=>getOperatorByKeyBoard(event)}>+</button>
+  
+            <button 
+            value="-" 
+                className={style? "buttonOperator": "buttonOperator-dark"}         
+                onClick={(event)=>getOperatorByKeyBoard(event)}>−</button>
+  
+          </div>
+        
+        </div>
+  
+          <button 
+                className={style? "buttonCero": "buttonCero-dark"}
+                value="0" 
+                onClick={(event)=>getNumByKeyBoard(event)}>0</button>
+          <button 
+                className={style? "button": "button-dark"}
+                value="." 
+                onClick={(event)=>getNumByKeyBoard(event)}>.</button>
+          <button  
+                className={style? "buttonOperator": "buttonOperator-dark"}         
+                onClick={getResult}>=</button> 
+        
+
+
+  
+      </div>
+  );
+  }
+  
